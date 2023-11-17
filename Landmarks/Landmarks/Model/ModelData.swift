1b0852878abb2d6d60e73c86455cb2906077f63b
Toheed Jahan Khan <ToheedKhan@users.noreply.github.com>
2023-11-17 16:42:25 +0530

    Created the row view.

--- Landmarks/Landmarks/Model/ModelData.swift
+++ Landmarks/Landmarks/Model/ModelData.swift
@@ -12,6 +12,9 @@ import Foundation
  
  The load method relies on the return type’s conformance to the Decodable protocol, which is one component of the Codable protocol.
  */
+
+var landmarks: [Landmark] = load("landmarkData.json")
+
 func load<T: Decodable>(_ filename: String) -> T {
     let data: Data
     
